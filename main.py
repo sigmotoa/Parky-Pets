6a99217efe0c4e620d8b740f5f2477c385e4317d
sigmotoa <si.galvismotoa@gmail.com>
2026-09-24 09:18:01 -0500

    [Add] The animal router has been added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from routers import user
+from routers import user, animal
 
 app = FastAPI()
 
 app.include_router(router=user.router, prefix="/users", tags=["users"])
+app.include_router(router=animal.router, prefix="/animals", tags=["animals"])
